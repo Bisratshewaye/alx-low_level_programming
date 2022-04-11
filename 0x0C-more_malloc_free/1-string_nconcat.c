dbab7c195140d16ec32fa4b056653019d18dd55d
Bisratshewaye <bisrishewa@gmail.com>
2022-04-11 21:00:29 +0300

    a function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,35 +1,51 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
- * string_nconcat - Concates two strings
- * @s1: The first string
- * @s2: The second string
- * @n: The number of byte to copy from s2.
- * Return: A pointer containing s1 + s2[0..n]
+ * string_nconcat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ * @n: index
+ * Return: char pointer
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, n1, n2;
-	char *res;
+	char *p;
+	unsigned int size1 = 0, size2 = 0, i;
 
-	if (!s1)
+	if (s1 == NULL)
 		s1 = "";
-	if (!s2)
+
+	if (s2 == NULL)
 		s2 = "";
-	for (n1 = 0; *(s1 + n1); n1++)
-		;
-	for (n2 = 0; *(s2 + n2); n2++)
-		;
-	if (n <= n2)
-		n2 = n;
-	res = malloc((n1 + n2 - 1) * sizeof(*res));
-	if (!res)
-		return (NULL);
-	for (i = 0; i < n1; i++)
-		res[i] = s1[i];
-	for (; i < n1 + n2; i++)
-		res[i] = s2[i - n1];
-	res[i] = '\0';
-	return (res);
+
+	while (s1[size1] != '\0')
+	{
+		size1++;
+	}
+
+	while (s2[size2] != '\0')
+	{
+		size2++;
+	}
+
+	if (n > size2)
+	n = size2;
+	p = malloc((size1 + n + 1) * sizeof(char));
+
+	if (p == NULL)
+		return (0);
+
+	for (i = 0; i < size1; i++)
+	{
+		p[i] = s1[i];
+	}
+
+	for (; i < (size1 + n); i++)
+	{
+		p[i] = s2[i - size1];
+	}
+	p[i] = '\0';
+
+return (p);
 }
